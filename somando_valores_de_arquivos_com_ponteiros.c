8f2705f4985f1655ee8c9f2302305f2ac69c024a
marcoatjunior <maarcojunioor@hotmail.com>
2017-12-03 21:13:55 -0200

    sum of integer values of a file with pointers

---
+++ somando_valores_de_arquivos_com_ponteiros.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ifIsunreadable(FILE * file){
+    if (file == NULL){
+        printf("Nao foi possivel abrir o arquivo.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int readIntValue(FILE * file, const char * name){
+    file = fopen(name, "r");
+    int i = 0, * values;
+
+    fscanf (file, "%d", &i);
+
+    while (!feof (file)){
+        fscanf (file, "%d", &i);
+        printf("%d", i);
+    }
+
+    fclose (file);
+
+    return values;
+}
+
+int main(){
+
+    FILE * fileOne;
+    FILE * fileTwo;
+    FILE * result;
+
+    int * vet1, * vet2, * values;
+    int i = 0;
+
+    // Verifica conteúdo dos arquivos
+    ifIsunreadable(fileOne);
+    ifIsunreadable(fileTwo);
+
+    // Recebe inteiros retornados do arquivo
+    vet1 = readIntValue(fileOne, "LP_arquivo_1.txt");
+    vet2 = readIntValue(fileTwo, "LP_arquivo_2.txt");
+
+    // Cria arquivo de resultado
+    result = fopen("marco_taborda_resultado.txt", "w");
+
+    // Verifica conteúdo do arquivo
+    ifIsunreadable(result);
+
+    // Adiciona ao ponteiro values cada valor das posições somadas
+    for(i = 0; i < 9; i++, vet1++, vet2++, values++){
+        * values = * vet1 + * vet2;
+        fprintf(result, * values);
+        strcat(* values, "\n");
+    }
+
+    // Fecha o arquivo
+    fclose(result);
+
+    // Finaliza o progrmada
+    return 0;
+}
