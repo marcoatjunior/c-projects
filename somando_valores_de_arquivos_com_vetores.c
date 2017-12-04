f69b9e6f78bb61181533d828866ca1ba02bf7b2b
marcoatjunior <maarcojunioor@hotmail.com>
2017-12-04 16:04:30 -0200

    sum of integer values of a file with arrays

--- somando_valores_de_arquivos_com_vetores.c
+++ somando_valores_de_arquivos_com_vetores.c
@@ -8,38 +8,34 @@ void ifIsUnreadable(FILE * file){
     }
 }
 
-int readIntValue(FILE * file, const char * name){
+void readIntValue(FILE * file, const char * name, int values[]){
     file = fopen(name, "r");
-    int i = 0, * values;
-
-    fscanf (file, "%d", &i);
+    int i = 0, pos = 0;
 
     while (!feof (file)){
         fscanf (file, "%d", &i);
-        printf("%d", i);
+        values[pos] = i;
+        pos++;
     }
 
     fclose (file);
-
-    return values;
 }
 
 int main(){
-
     FILE * fileOne;
     FILE * fileTwo;
     FILE * result;
 
-    int * vet1, * vet2, * values;
-    int i = 0;
+    int i = 0, * sum;
+    int values1[10], values2[10], results[10];
 
     // Verifica conteúdo dos arquivos
     ifIsUnreadable(fileOne);
     ifIsUnreadable(fileTwo);
 
     // Recebe inteiros retornados do arquivo
-    vet1 = readIntValue(fileOne, "LP_arquivo_1.txt");
-    vet2 = readIntValue(fileTwo, "LP_arquivo_2.txt");
+    readIntValue(fileOne, "LP_arquivo_1.txt", values1);
+    readIntValue(fileTwo, "LP_arquivo_2.txt", values2);
 
     // Cria arquivo de resultado
     result = fopen("marco_taborda_resultado.txt", "w");
@@ -47,16 +43,19 @@ int main(){
     // Verifica conteúdo do arquivo
     ifIsUnreadable(result);
 
-    // Adiciona ao ponteiro values cada valor das posições somadas
-    for(i = 0; i < 9; i++, vet1++, vet2++, values++){
-        * values = * vet1 + * vet2;
-        fprintf(result, * values);
-        strcat(* values, "\n");
+    sum = &results[0];
+
+    // Adiciona ao vetor results cada valor das posições somadas
+    for(i = 0; i < 9; i++, sum++){
+        results[i] = values1[i] + values2[i];
+        printf("Soma Posicao %d = %d \n", i+1, results[i]);
+        fprintf(result, sum);
+        strcat(sum, "\n");
     }
 
     // Fecha o arquivo
     fclose(result);
 
-    // Finaliza o progrmada
+    // Finaliza o programa
     return 0;
 }
